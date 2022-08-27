6bde0844d8b7d18333118c3f76f4d74089100b0b
Lee <li20020410@126.com>
2022-08-27 23:23:39 +0800

    Add gamma correction

--- utils/color.h
+++ utils/color.h
@@ -8,9 +8,18 @@ typedef vec3 color;
 
 std::tuple<short, short, short> color_to_RGB(const color & pixel)
 {
-    return std::make_tuple(static_cast<int>(255.999 * pixel[0]),
-                           static_cast<int>(255.999 * pixel[1]),
-                           static_cast<int>(255.999 * pixel[2])
+    double r = pixel[0];
+    double g = pixel[1];
+    double b = pixel[2];
+
+    // Gamma correction using gamma=2.0
+    r = std::sqrt(r);
+    g = std::sqrt(g);
+    b = std::sqrt(b);
+
+    return std::make_tuple(static_cast<int>(256 * tools::clamp(r, 0.0, 0.99)),
+                           static_cast<int>(256 * tools::clamp(g, 0.0, 0.99)),
+                           static_cast<int>(256 * tools::clamp(b, 0.0, 0.99))
                            );
 }
 
