5279c741ef5f54c045567b1086f754ab8a19b273
Lee <li20020410@126.com>
2022-08-29 23:48:52 +0800

    Merge definition of lambertian scatter function with its declaration to avoid redef

--- material/lambertian_simple.h
+++ material/lambertian_simple.h
@@ -13,24 +13,22 @@ public:
     virtual bool evaluateScatter(const ray & incident,
                                  const hit_record & rec,
                                  color & attenuation,
-                                 ray & scattered) const override;
+                                 ray & scattered) const override
+    {
+        // PDF of random_unit() is cos(theta) / PI
+        // Where theta is the angle between normal and scattered ray
+        auto scatter_dir = rec.normal + vec3::random_unit();
+
+        if(tools::is_near_zero(scatter_dir))
+            scatter_dir = rec.normal;
+
+        scattered = ray(rec.p, scatter_dir);
+        attenuation = k_L;
+        return true;
+    }
 private:
     color k_L;
 };
-
-bool lambertian::evaluateScatter([[maybe_unused]] const ray& incident, const hit_record& rec, color& attenuation, ray& scattered) const
-{
-    // PDF of random_unit() is cos(theta) / PI
-    // Where theta is the angle between normal and scattered ray
-    auto scatter_dir = rec.normal + vec3::random_unit();
-
-    if(tools::is_near_zero(scatter_dir))
-        scatter_dir = rec.normal;
-
-    scattered = ray(rec.p, scatter_dir);
-    attenuation = k_L;
-    return true;
-}
 
 
 #endif // LAMBERTIAN_SIMPLE_H_INCLUDED
