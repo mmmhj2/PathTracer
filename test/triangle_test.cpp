013e17006117c6c84aba12300342782cce770a31
Lee <li20020410@126.com>
2022-09-01 23:09:37 +0800

    Add cornell box testing

--- test/triangle_test.cpp
+++ test/triangle_test.cpp
@@ -5,6 +5,7 @@
 #include "material/lambertian_simple.h"
 #include "material/texture/texture_base.h"
 #include "material/texture/checker_texture.h"
+#include "material/texture/image_texture.h"
 #include "objects/triangle_flat.h"
 #include "objects/sphere.h"
 #include "objects/spatial_structure/bvh/bvh.h"
@@ -65,45 +66,84 @@ void test_simple()
 
 int main()
 {
-    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), constants::pi / 9);
+    camera cam(point3(278,278,1500), point3(278,278,0), vec3(0,1,0), constants::pi / 4.5, 1.0);
     skybox_color_gradient sky;
     bvh_tree world;
 
-    vec3 vert[3] =
+    vec3 vert_right[] =
+    {
+        vec3(0, 0, 555),
+        vec3(0, 0, 0),
+        vec3(0, 555, 0),
+        vec3(0, 555, 555),
+        vec3(0, 0, 555)
+    };
+    vec3 vert_left[] =
+    {
+        vec3(555, 0, 555),
+        vec3(555, 0, 0),
+        vec3(555, 555, 0),
+        vec3(555, 555, 555),
+        vec3(555, 0, 555)
+    };
+    vec3 vert_back[] =
     {
         vec3(0, 0, 0),
-        vec3(1, 0, 0),
-        vec3(0, 1, 0)
+        vec3(555, 0, 0),
+        vec3(555, 555, 0),
+        vec3(0, 555, 0),
+        vec3(0, 0, 0)
+    };
+    vec3 vert_down[] =
+    {
+        vec3(0, 0, 555),
+        vec3(555, 0, 555),
+        vec3(555, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 555)
     };
 
-    std::pair <double, double> uv[3] =
+    std::pair <double, double> uv[5] =
     {
         std::make_pair(0.0, 0.0),
         std::make_pair(1.0, 0.0),
-        std::make_pair(0.0, 1.0)
+        std::make_pair(1.0, 1.0),
+        std::make_pair(0.0, 1.0),
+        std::make_pair(0.0, 0.0)
     };
 
-    auto texture_ground = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
-    //auto texture_up = make_shared<solid_color>(color(20, 20, 20));
-    auto texture_center = make_shared<solid_color>(color(0.4, 0, 0));
+    auto red_tex   = make_shared<solid_color>(color(.65, .05, .05));
+    auto white_tex = make_shared<solid_color>(color(.73, .73, .73));
+    auto green_tex = make_shared<solid_color>(color(.12, .45, .15));
+    auto back_tex = make_shared<image_texture>("back.jpg");
+    auto red   = make_shared<lambertian>(red_tex);
+    auto white = make_shared<lambertian>(white_tex);
+    auto green = make_shared<lambertian>(green_tex);
+    auto back_mat = make_shared<lambertian>(back_tex);
 
-    auto material_ground = make_shared<lambertian>(texture_ground);
-    //auto material_up = make_shared<emissive_diffuse>(texture_up);
-    //auto material_center = make_shared<blinn_phong_naive>(color(0.4, 0, 0), color(1, 1, 1), 10);
-    auto material_center = make_shared<lambertian>(texture_center);
-
-    world.add_object(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    //world.add_object(make_shared<sphere>(point3( 0.0,    1.0, -1.0),   0.25, material_up));
-    world.add_object(make_shared<triangle_flat>(vert, uv, material_center));
+    world.add_object(make_shared<triangle_flat>(vert_right, uv, green));
+    world.add_object(make_shared<triangle_flat>(vert_right+2, uv+2, green));
+    world.add_object(make_shared<triangle_flat>(vert_left, uv, red));
+    world.add_object(make_shared<triangle_flat>(vert_left+2, uv+2, red));
+    world.add_object(make_shared<triangle_flat>(vert_back, uv, back_mat));
+    world.add_object(make_shared<triangle_flat>(vert_back+2, uv+2, back_mat));
+    world.add_object(make_shared<triangle_flat>(vert_down, uv, white));
+    world.add_object(make_shared<triangle_flat>(vert_down+2, uv+2, white));
+    //world.add_object(make_shared<sphere>(point3( 0.0, 0.0, 0.0), 200, white));
     world.build();
 
     std::vector <concurrent::block_info> infos(constants::blocks);
-    int scanline_per_blocks = constants::image_height / constants::blocks + 1;
+    int image_width = 600;
+    int image_height = 600;
+
+    int scanline_per_blocks = image_height / constants::blocks + 1;
     for(int i = 0; i < constants::blocks; i++)
     {
-        infos[i].scanline_max = constants::image_height - i * scanline_per_blocks;
-        const int scanline_min = constants::image_height - (i+1) * scanline_per_blocks;
+        infos[i].scanline_max = image_height - i * scanline_per_blocks;
+        const int scanline_min = image_height - (i+1) * scanline_per_blocks;
         infos[i].scanline_min = std::max(0, scanline_min);
+        infos[i].image_height = image_height;
+        infos[i].image_width = image_width;
         cout << "Block " << i << " from " << infos[i].scanline_max << " to " << infos[i].scanline_min << endl ;
         infos[i].cam = &cam;
         infos[i].skybox = &sky;
@@ -144,6 +184,6 @@ int main()
 
     ofstream fout;
     fout.open("triangle.ppm");
-    ImageOutputPPM()(constants::image_width, constants::image_height, fout, pic);
+    ImageOutputPPM()(image_width, image_height, fout, pic);
     fout.close();
 }
