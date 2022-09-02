4a82537d09f39141a9cdc2a27680079301ea691d
Lee <li20020410@126.com>
2022-09-02 17:19:21 +0800

    Add progress bar and test

---
+++ test/pbar_test.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+#include <future>
+//#define TRIANGLE_TEST
+#include "material/lambertian_simple.h"
+#include "material/texture/texture_base.h"
+#include "material/texture/checker_texture.h"
+#include "material/texture/image_texture.h"
+#include "objects/triangle_flat.h"
+#include "objects/sphere.h"
+#include "objects/spatial_structure/bvh/bvh.h"
+#include "ray/camera.h"
+#include "skybox/skybox_colored.h"
+#include "tracer/tracer.h"
+#include "image/ImageOutputPPM.h"
+
+using namespace std;
+
+int main()
+{
+    camera cam(point3(278,278,1500), point3(278,278,0), vec3(0,1,0), constants::pi / 4.5, 1.0);
+    skybox_color_gradient sky;
+    bvh_tree world;
+
+    vec3 vert_right[] =
+    {
+        vec3(0, 0, 555),
+        vec3(0, 0, 0),
+        vec3(0, 555, 0),
+        vec3(0, 555, 555),
+        vec3(0, 0, 555)
+    };
+    vec3 vert_left[] =
+    {
+        vec3(555, 0, 555),
+        vec3(555, 0, 0),
+        vec3(555, 555, 0),
+        vec3(555, 555, 555),
+        vec3(555, 0, 555)
+    };
+    vec3 vert_back[] =
+    {
+        vec3(0, 0, 0),
+        vec3(555, 0, 0),
+        vec3(555, 555, 0),
+        vec3(0, 555, 0),
+        vec3(0, 0, 0)
+    };
+    vec3 vert_down[] =
+    {
+        vec3(0, 0, 555),
+        vec3(555, 0, 555),
+        vec3(555, 0, 0),
+        vec3(0, 0, 0),
+        vec3(0, 0, 555)
+    };
+
+    std::pair <double, double> uv[5] =
+    {
+        std::make_pair(0.0, 0.0),
+        std::make_pair(1.0, 0.0),
+        std::make_pair(1.0, 1.0),
+        std::make_pair(0.0, 1.0),
+        std::make_pair(0.0, 0.0)
+    };
+
+    auto red_tex   = make_shared<solid_color>(color(.65, .05, .05));
+    auto white_tex = make_shared<solid_color>(color(.73, .73, .73));
+    auto green_tex = make_shared<solid_color>(color(.12, .45, .15));
+    auto back_tex = make_shared<image_texture>("back.jpg");
+    auto red   = make_shared<lambertian>(red_tex);
+    auto white = make_shared<lambertian>(white_tex);
+    auto green = make_shared<lambertian>(green_tex);
+    auto back_mat = make_shared<lambertian>(back_tex);
+
+    world.add_object(make_shared<triangle_flat>(vert_right, uv, green));
+    world.add_object(make_shared<triangle_flat>(vert_right+2, uv+2, green));
+    world.add_object(make_shared<triangle_flat>(vert_left, uv, red));
+    world.add_object(make_shared<triangle_flat>(vert_left+2, uv+2, red));
+    world.add_object(make_shared<triangle_flat>(vert_back, uv, back_mat));
+    world.add_object(make_shared<triangle_flat>(vert_back+2, uv+2, back_mat));
+    world.add_object(make_shared<triangle_flat>(vert_down, uv, white));
+    world.add_object(make_shared<triangle_flat>(vert_down+2, uv+2, white));
+
+    world.build();
+
+    std::vector <concurrent::block_info> infos(constants::blocks);
+    int image_width = 600;
+    int image_height = 600;
+
+    int scanline_per_blocks = image_height / constants::blocks + 1;
+    for(int i = 0; i < constants::blocks; i++)
+    {
+        infos[i].scanline_max = image_height - i * scanline_per_blocks;
+        const int scanline_min = image_height - (i+1) * scanline_per_blocks;
+        infos[i].scanline_min = std::max(0, scanline_min);
+        infos[i].image_height = image_height;
+        infos[i].image_width = image_width;
+        cout << "Block " << i << " from " << infos[i].scanline_max << " to " << infos[i].scanline_min << endl ;
+        infos[i].cam = &cam;
+        infos[i].skybox = &sky;
+        infos[i].world = &world;
+        infos[i].progress = 0;
+    }
+
+    std::vector <std::future<std::vector<color>>> async_task;
+    for(int i = 0; i < constants::blocks; i++)
+        async_task.push_back(std::async(concurrent::trace_block, std::cref(infos[i])));
+
+    pbar::init_curse();
+    while(true)
+    {
+        if(pbar::print_progressbar(infos))
+            break;
+        std::this_thread::sleep_for(200ms);
+    }
+    cout << endl ;
+
+    std::vector <color> pic;
+    for(int i = 0; i < constants::blocks; i++)
+    {
+        std::vector <color> block = async_task[i].get();
+        pic.insert(pic.end(), block.cbegin(), block.cend());
+    }
+
+
+    ofstream fout;
+    fout.open("pbar.ppm");
+    ImageOutputPPM()(image_width, image_height, fout, pic);
+    fout.close();
+}
---
+++ tracer/progress_bar.h
@@ -0,0 +1,61 @@
+#ifndef PROGRESS_BAR_H_INCLUDED
+#define PROGRESS_BAR_H_INCLUDED
+
+#include "concurrent_trace.h"
+#include <cinttypes>
+
+namespace pbar
+{
+
+#include <ncurses/ncurses.h>
+
+void init_curse()
+{
+    initscr();
+    noecho();
+    start_color();
+
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+}
+
+bool print_progressbar(const std::vector <concurrent::block_info> & info)
+{
+    constexpr int name_witdh = 15;
+    constexpr int bar_width = 50;
+    bool is_completed = true;
+    clear();
+    for(auto itr = info.cbegin(); itr != info.cend(); ++itr)
+    {
+        int i = std::distance(info.cbegin(), itr);
+        bool not_completed = itr->progress < itr->scanline_max - itr->scanline_min;
+
+        if(!not_completed)
+            attron(COLOR_PAIR(2));
+        else
+            attron(COLOR_PAIR(0));
+
+        mvprintw(i, 0, "THREAD %02d" , i+1);
+
+        mvaddch(i, name_witdh, '[');
+        mvhline(i, name_witdh + 1, '=', itr->progress * bar_width / (itr->scanline_max - itr->scanline_min));
+        mvaddch(i, name_witdh + bar_width + 1, ']');
+
+        mvprintw(i, name_witdh + bar_width + 5, "%2d of %2d", itr->progress.load(), itr->scanline_max - itr->scanline_min);
+
+        if(not_completed)
+        {
+            is_completed = false;
+            attroff(COLOR_PAIR(0));
+        }
+        else
+            attroff(COLOR_PAIR(2));
+
+    }
+    refresh();
+    return is_completed;
+}
+
+};
+
+#endif // PROGRESS_BAR_H_INCLUDED
