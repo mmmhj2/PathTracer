8cdc0f58546afc3b58ee81c70391ce3f05dbbb0c
Lee <li20020410@126.com>
2022-09-03 15:07:53 +0800

    Update test file

--- test/pdf_test.cpp
+++ test/pdf_test.cpp
@@ -1,21 +1,28 @@
 #include "utils/orthonormal_basis.h"
 #include "material/pdf/pdf.h"
+#include "objects/triangle_flat.h"
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    pdf_cosine pc(vec3(0, 0, 1));
-    pdf_uniform pu(vec3(0, 0, 1));
-
-    cout << "Test;" << endl ;
-    cout << "::X::Y::Z;" << endl ;
-    for(int i = 0; i < 5000; i++)
+    vec3 vert[3] =
     {
-        vec3 v = pc.sample_hemisphere();
-        cout << i << "::" << v[0] << "::" << v[1] << "::" << v[2] << "::0::1::A::1::0::0::0::0;" << endl ;
-        v = pu.sample_hemisphere();
-        cout << i << "::" << v[0] << "::" << v[1] << "::" << v[2] << "::0::1::B::1::0::0::0::0;" << endl ;
-    }
+        vec3(0, 0, 0),
+        vec3(1, 0, 0),
+        vec3(0, 1, 0)
+    };
+    std::pair <double, double> uv[3] =
+    {
+        std::make_pair(0, 0),
+        std::make_pair(1, 0),
+        std::make_pair(0, 1)
+    };
 
+    auto triangle = std::make_shared<triangle_flat>(vert, uv, nullptr);
+
+    for(int i = 0; i <= 100; i++)
+        cout << triangle->sample(vec3(0, 0, 1)) << endl ;
+    return 0;
 }
