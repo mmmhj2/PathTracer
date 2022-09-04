03a5e841b1f1fe21315c39f4dafea5bcf6b4f396
Lee <li20020410@126.com>
2022-09-04 15:42:58 +0800

    Update dielectric to use BSDF

--- material/dielectric.h
+++ material/dielectric.h
@@ -7,44 +7,19 @@ class dielectric : public non_emissive_material
 {
 private:
     double ior;
-    color albedo;
-
-    static double schlick_approximation(double cosine, double ior)
-    {
-        double r0 = (1 - ior) / (1 + ior);
-        r0 *= r0;
-        return r0 + (1-r0) * std::pow((1-cosine), 5);
-    }
+    std::shared_ptr <uv_texture> albedo;
 
 public:
-    dielectric(double index_of_refraction, color alb = color(1.0, 1.0, 1.0))
+    dielectric(double index_of_refraction, std::shared_ptr <uv_texture> alb)
     : ior(index_of_refraction), albedo(alb) {};
     virtual bool evaluateScatter(const ray & incident,
                                  const hit_record & rec,
-                                 color & attenuation,
-                                 ray & scattered) const override;
+                                 std::shared_ptr <BSDF_base> & bsdf) const override;
 };
 
-bool dielectric::evaluateScatter(const ray& incident, const hit_record& rec, color& attenuation, ray& scattered) const
+bool dielectric::evaluateScatter(const ray& incident, const hit_record& rec, std::shared_ptr <BSDF_base> & bsdf) const
 {
-    attenuation = albedo;
-    double actual_ior = rec.is_front ? (1.0 / ior) : ior;
-
-    vec3 unit_direction = incident.direction().unit();
-    vec3 direction;
-
-    double cos_theta = std::min((-unit_direction) * rec.normal, 1.0);
-    double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
-
-    bool is_reflect =
-        schlick_approximation(cos_theta, actual_ior) > tools::random_double()
-        || actual_ior * sin_theta > 1.0;
-    if(is_reflect)
-        direction = tools::reflect(unit_direction, rec.normal);
-    else
-        direction = tools::refract(unit_direction, rec.normal, actual_ior);
-
-    scattered = ray(rec.p, direction);
+    bsdf = std::make_shared<BSDF_specular_refraction>(rec, ior, albedo);
     return true;
 }
 
--- test/specular_test.cpp
+++ test/specular_test.cpp
@@ -10,37 +10,37 @@
 #include "ray/camera.h"
 #include "tracer/tracer.h"
 #include "material/lambertian_simple.h"
-//#include "material/dielectric.h"
+#include "material/dielectric.h"
 #include "material/metallic.h"
 #include "material/texture/checker_texture.h"
 using namespace std;
 
 int main()
 {
-    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), constants::pi / 9, 1.0);
+    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), constants::pi / 9);
     skybox_color_gradient sky;
     bvh_tree world;
 
     auto texture_ground = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
     auto texture_center = make_shared<solid_color>(color(0.1, 0.2, 0.5));
-    auto texture_right = make_shared<solid_color>(color(1, 1, 1));
+    auto texture_full_white = make_shared<solid_color>(color(1, 1, 1));
 
     auto material_ground = make_shared<lambertian>(texture_ground);
     auto material_center = make_shared<lambertian>(texture_center);
-    //auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metallic>(texture_right);
+    auto material_left   = make_shared<dielectric>(1.5, texture_full_white);
+    auto material_right  = make_shared<metallic>(texture_full_white);
 
     world.add_object(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add_object(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    //world.add_object(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    //world.add_object(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
+    world.add_object(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add_object(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
     world.add_object(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     world.build();
 
     std::vector <concurrent::block_info> infos(constants::blocks);
     int image_width = 600;
-    int image_height = 600;
+    int image_height = image_width / constants::aspect_ratio;
 
     concurrent::fill_info(infos, image_width, image_height, constants::blocks, &cam, &world, &sky, nullptr);
 
