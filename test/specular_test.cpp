106653299765f636b7bb4626e2ed77f9eceb7613
Lee <li20020410@126.com>
2022-09-04 15:21:05 +0800

    Update specular test

--- test/specular_test.cpp
+++ test/specular_test.cpp
@@ -1,24 +1,73 @@
 #include <iostream>
-#define BLINN_PHONG_DEBUG
-#include "material/blinn_phong.h"
+#include <fstream>
+#include <future>
 
+#include "utils/defs.h"
+#include "image/ImageOutputPPM.h"
+#include "skybox/skybox_colored.h"
+#include "objects/sphere.h"
+#include "objects/spatial_structure/bvh/bvh.h"
+#include "ray/camera.h"
+#include "tracer/tracer.h"
+#include "material/lambertian_simple.h"
+//#include "material/dielectric.h"
+#include "material/metallic.h"
+#include "material/texture/checker_texture.h"
 using namespace std;
 
 int main()
 {
+    camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), constants::pi / 9, 1.0);
+    skybox_color_gradient sky;
+    bvh_tree world;
 
-    auto mat = std::make_shared<blinn_phong_naive> (color(0, 0, 0), color(0, 0, 0), 1);
+    auto texture_ground = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    auto texture_center = make_shared<solid_color>(color(0.1, 0.2, 0.5));
+    auto texture_right = make_shared<solid_color>(color(1, 1, 1));
 
-    ray test_ray;
-    hit_record test_hit;
-    color result;
-    ray scattered;
+    auto material_ground = make_shared<lambertian>(texture_ground);
+    auto material_center = make_shared<lambertian>(texture_center);
+    //auto material_left   = make_shared<dielectric>(1.5);
+    auto material_right  = make_shared<metallic>(texture_right);
 
-    test_ray.orig = vec3(0, 1, -1);
-    test_ray.dir = vec3(0, -1, 1);
-    test_hit.p = point3(0, 0, 0);
-    test_hit.normal = vec3(0, 1, 0);
+    world.add_object(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add_object(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    //world.add_object(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    //world.add_object(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
+    world.add_object(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
-    for(int i = 0 ; i < 50; i++)
-        mat->evaluateScatter(test_ray, test_hit, result, scattered);
+    world.build();
+
+    std::vector <concurrent::block_info> infos(constants::blocks);
+    int image_width = 600;
+    int image_height = 600;
+
+    concurrent::fill_info(infos, image_width, image_height, constants::blocks, &cam, &world, &sky, nullptr);
+
+    std::vector <std::future<std::vector<color>>> async_task;
+    for(int i = 0; i < constants::blocks; i++)
+        async_task.push_back(std::async(concurrent::trace_block, std::cref(infos[i])));
+
+    pbar::init_curses();
+    while(true)
+    {
+        if(pbar::print_progressbar(infos))
+            break;
+        std::this_thread::sleep_for(200ms);
+    }
+    pbar::decon_curses();
+    cout << endl ;
+
+    std::vector <color> pic;
+    for(int i = 0; i < constants::blocks; i++)
+    {
+        std::vector <color> block = async_task[i].get();
+        pic.insert(pic.end(), block.cbegin(), block.cend());
+    }
+
+
+    ofstream fout;
+    fout.open("specular.ppm");
+    ImageOutputPPM()(image_width, image_height, fout, pic);
+    fout.close();
 }
